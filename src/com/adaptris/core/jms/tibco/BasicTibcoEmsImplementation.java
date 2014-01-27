4d05ebeb77be38f733a6efb961263eacb049a17a
Lewin Chan <lewin.chan@adaptris.com>
2014-01-27 12:42:44 +0000

    redmineID #6285 - Tibco EMS Implementations now extend UrlVendorImp

--- src/com/adaptris/core/jms/tibco/BasicTibcoEmsImplementation.java
+++ src/com/adaptris/core/jms/tibco/BasicTibcoEmsImplementation.java
@@ -3,31 +3,28 @@ package com.adaptris.core.jms.tibco;
 import javax.jms.ConnectionFactory;
 import javax.jms.JMSException;
 
-import com.adaptris.core.jms.JmsConnectionConfig;
-import com.adaptris.core.jms.VendorImplementationImp;
+import com.adaptris.core.jms.UrlVendorImplementation;
 import com.tibco.tibjms.TibjmsQueueConnectionFactory;
 
 /**
  * <p>
  * <code>BasicTibcoEmsImplementation</code>
  * </p>
- *
- * @author Aaron - 5 Oct 2012
- * @version
- *
  * <p>
- * In the adapter configuration file this class is aliased as <b>BasicTibcoEmsImplementation</b> which is the preferred alternative to the
- * fully qualified classname when building your configuration.
+ * In the adapter configuration file this class is aliased as <b>BasicTibcoEmsImplementation</b> which is the preferred alternative
+ * to the fully qualified classname when building your configuration.
  * </p>
+ * 
+ * @author Aaron - 5 Oct 2012
  */
-public class BasicTibcoEmsImplementation extends VendorImplementationImp {
+public class BasicTibcoEmsImplementation extends UrlVendorImplementation {
 
-	@Override
-	public ConnectionFactory createConnectionFactory(JmsConnectionConfig cfg) throws JMSException {
-    return createConnectionFactory(cfg.configuredBrokerUrl());
-	}
+  @Override
+  public ConnectionFactory createConnectionFactory() throws JMSException {
+    return createConnectionFactory(getBrokerUrl());
+  }
 
-	protected ConnectionFactory createConnectionFactory(String serverUrl) {
-		return new TibjmsQueueConnectionFactory(serverUrl);
-	}
+  protected ConnectionFactory createConnectionFactory(String serverUrl) {
+    return new TibjmsQueueConnectionFactory(serverUrl);
+  }
 }
